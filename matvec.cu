b2259259ed0073c24857d74a3ea81074d22a54d3
slinky57 <slinky57@protonmail.com>
2020-12-05 13:25:01 -0700

    Update matvec.cu

--- matvec.cu
+++ matvec.cu
@@ -16,6 +16,31 @@ __global__ void matrixMultKernel(float* A, float* B, float* C, int n)
     }
 }
 
+matVecMultKernel(float* A, float* B, float* C, int n)
+{
+    int col = blockIdx.x*blockDim.x + threadIdx.x;
+    int row = blockIdx.y*blockDim.y + threadIdx.y;
+    
+    if (row < n && col < n)
+    {
+        C[row] = A[row*n + col] * B[col];
+    }
+}
+
+void matVecMult(float* A, float* B, float* C, int n)
+{
+    float val;
+    for (int i=0; i<n; i++)
+    {
+        val = 0;
+        for (int j=0; j<n; j++)
+        {
+            val += A[i*n+j] * B[j];
+        }
+        C[i] = val;
+    }
+}
+
 void matrixMult(float* A, float* B, float* C, int n)
 {
     float val;
@@ -40,6 +65,14 @@ double sum(float* C, int n)
     return s;
 }
 
+double vecSum(float* C, int n)
+{
+    double s = 0;
+    for (int i = 0; i < n; i++)
+        s += C[i]
+    return s
+}
+
 int main(int argc, char* argv[])
 {
     float *h_A, *h_B, *h_C;
@@ -48,6 +81,7 @@ int main(int argc, char* argv[])
 
     int n = atoi(argv[1]);
     int size = n*n*sizeof(float);
+    int vecsize=n*sizeof(float);
     int numTests = atoi(argv[2]);
 
 ///
@@ -58,12 +92,12 @@ int main(int argc, char* argv[])
 ///DA
 
     h_A = (float*)malloc(size);
-    h_B = (float*)malloc(size);
-    h_C = (float*)malloc(size);
+    h_B = (float*)malloc(vecsize);
+    h_C = (float*)malloc(vecsize);
     
     cudaMalloc((void**)&d_A, size);
-    cudaMalloc((void**)&d_B, size);
-    cudaMalloc((void**)&d_C, size);
+    cudaMalloc((void**)&d_B, vecsize);
+    cudaMalloc((void**)&d_C, vecsize);
 
 
    for (int test=0;test<numTests;test++)
@@ -71,20 +105,25 @@ int main(int argc, char* argv[])
         for (int i = 0; i < n*n; i++)
         {
             h_A[i] = (float) rand();
-            h_B[i] = (float) rand();
+            if (i<n) h_B[i] = (float) rand();
         }
         // Matmat
         dim3 dimBlock(32,32);
         int grid_dim = ceil(n / 32.0);
         dim3 dimGrid(grid_dim, grid_dim);
+        
+        t0 = get_time();
+        matVecMult(h_A, h_B, h_C, n);
+        tfinal = get_time() - t0;
+        printf("MatVecMult Time %e, Sum %e\n", tfinal, vecSum(h_C, n));
 
         t0 = get_time();
         cudaMemcpy(d_A, h_A, size, cudaMemcpyHostToDevice);
         cudaMemcpy(d_B, h_B, size, cudaMemcpyHostToDevice);
-        matrixMultKernel<<<dimGrid, dimBlock>>>(d_A, d_B, d_C, n);
+        matVecMultKernel<<<dimGrid, dimBlock>>>(d_A, d_B, d_C, n);
         cudaMemcpy(h_C, d_C, size, cudaMemcpyDeviceToHost);
         tfinal = get_time() - t0;
-        printf("MatrixMultKernel Time %e, Size %e\n", tfinal, sum(h_C, n));
+        printf("MatrixMultKernel Time %e, Size %e\n", tfinal, vecSum(h_C, n));
 
     ///
         //save time
