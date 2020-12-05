30fd5d57be7bf4763e891073b7a8123233802f27
slinky57 <slinky57@protonmail.com>
2020-12-05 15:22:32 -0700

    Update matvec.cu

--- matvec.cu
+++ matvec.cu
@@ -32,29 +32,6 @@ void matVecMult(float* A, float* B, float* C, int n)
     }
 }
 
-void matrixMult(float* A, float* B, float* C, int n)
-{
-    float val;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            val = 0;
-            for (int k = 0; k < n; k++)
-                val += A[i*n+k] * B[k*n+j];
-            C[i*n+j] = val;
-        }
-    }
-}
-
-double sum(float* C, int n)
-{
-    double s = 0;
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            s += C[i*n+j];
-    return s;
-}
 
 double vecSum(float* C, int n)
 {
@@ -71,22 +48,20 @@ int main(int argc, char* argv[])
     double t0, tfinal;
 
     int n = atoi(argv[1]);
-    int size = n*n*sizeof(float);
+    int matsize = n*n*sizeof(float);
     int vecsize=n*sizeof(float);
     int numTests = atoi(argv[2]);
 
-///
     //make file
     FILE * fPtr;
     char fPath[100];
     sprintf(fPath,"Problem2/matvec_N_%d.txt",n);
-///DA
 
-    h_A = (float*)malloc(size);
+    h_A = (float*)malloc(matsize);
     h_B = (float*)malloc(vecsize);
     h_C = (float*)malloc(vecsize);
     
-    cudaMalloc((void**)&d_A, size);
+    cudaMalloc((void**)&d_A, matsize);
     cudaMalloc((void**)&d_B, vecsize);
     cudaMalloc((void**)&d_C, vecsize);
 
@@ -99,27 +74,24 @@ int main(int argc, char* argv[])
             if (i<n) h_B[i] = (float) rand();
         }
         
-        t0 = get_time();
-        matVecMult(h_A, h_B, h_C, n);
-        tfinal = get_time() - t0;
-        printf("MatVecMult Time %e, Sum %e\n", tfinal, vecSum(h_C, n));
+//        t0 = get_time();
+//        matVecMult(h_A, h_B, h_C, n);
+//        tfinal = get_time() - t0;
+//        printf("MatVecMult Time %e, Sum %e\n", tfinal, vecSum(h_C, n));
 
         t0 = get_time();
-        cudaMemcpy(d_A, h_A, size, cudaMemcpyHostToDevice);
+        cudaMemcpy(d_A, h_A, matsize, cudaMemcpyHostToDevice);
         cudaMemcpy(d_B, h_B, vecsize, cudaMemcpyHostToDevice);
         matVecMultKernel<<<ceil(n/256.0), 256>>>(d_A, d_B, d_C, n);
         cudaMemcpy(h_C, d_C, vecsize, cudaMemcpyDeviceToHost);
         tfinal = get_time() - t0;
         printf("MatVecMultKernel Time %e, Sum %e\n", tfinal, vecSum(h_C, n));
 
-    ///
         //save time
         fPtr = fopen(fPath ,"a");
         if (fPtr == NULL) exit(EXIT_FAILURE);
         fprintf(fPtr,"%e\n",tfinal);
         fclose(fPtr);
-    ///DA
-
         
    }
     cudaFree(d_A);
